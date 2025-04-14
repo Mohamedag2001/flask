6722ba60119e4b852f0b8820d491df6d0b952754
Mohamedag2001 <mohaansar2001@gmail.com>
2025-04-14 02:37:06 +0000

    two

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-@app.route("/accueil")
+@app.route("/")
 def index():
-    return render_template("index.html", title="Accueil")
+    return render_template("index.html")
 
-@app.route("/apropos")
+@app.route("/about/")
 def about():
-    return render_template("about.html", title="À propos")
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
